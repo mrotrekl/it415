e7426f0fc43fdc88d44f20f5f7d6a888012ddf77
Marek Rotrekl <bbckt@rotrekl.name>
2020-01-19 23:12:13 +0100

    TaskTest final, not attached exc remaining

--- src/test/java/cz/xrotm01/rukovoditel/selenium/TaskTest.java
+++ src/test/java/cz/xrotm01/rukovoditel/selenium/TaskTest.java
@@ -24,7 +24,7 @@ public class TaskTest {
     public void init() {
         ChromeOptions cho = new ChromeOptions();
 
-        boolean runOnTravis = false;
+        boolean runOnTravis = true;
         if (runOnTravis) {
             cho.addArguments("headless");
         } else {
@@ -37,7 +37,7 @@ public class TaskTest {
 
     @After
     public void tearDown() {
-//        driver.close();
+        driver.close();
     }
 
 
@@ -60,22 +60,19 @@ public class TaskTest {
 
         //najit radek, kliknout na nej
 
-   /*     List<WebElement> menuRows = driver.findElements(By.cssSelector("ul.page-sidebar-menu>li"));
+        List<WebElement> menuRows = driver.findElements(By.cssSelector("ul.page-sidebar-menu>li"));
         for (WebElement e: menuRows) {
             if (e.getText().equals("Projects")) {
                 e.click();
             }
-        } */
-
-        driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/items&path=21");
+        }
 
         // je treba odstranit problem s exception: element is not attached to the page document
         // pokud se pouzije vyse primo tento odkaz, problem nenastava driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/items&path=21");
 
         WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".reset_search")));
-
         WebElement resetSearch = driver.findElement(By.cssSelector(".reset_search"));
+        wait.until(ExpectedConditions.visibilityOf(resetSearch));
         resetSearch.click();
 
         WebElement searchInput = driver.findElement(By.id("entity_items_listing66_21_search_keywords"));
@@ -84,27 +81,25 @@ public class TaskTest {
         WebElement projectButton2 = driver.findElement(By.className("btn-info"));
         projectButton2.click();
 
-     /*   WebDriverWait wait2 = new WebDriverWait(driver, 5);
-        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".item_heading_link")));
-
+        WebDriverWait wait2 = new WebDriverWait(driver, 10);
         WebElement projectLink = driver.findElement(By.cssSelector(".item_heading_link"));
-       // projectLink = driver.findElement(By.cssSelector(".item_heading_link")); //prvni nacteni nefunguje
-        projectLink.click();*/
-
-        driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/info&path=21-2792&redirect_to=subentity&gotopage[66]=1");
-        // z duvodu not attached to the document exception
+        wait2.until(ExpectedConditions.visibilityOf(projectLink));
+        projectLink.click();
+        // je treba odstranit problem s exception: element is not attached to the page document
+        // pokud se pouzije vyse primo tento odkaz, problem nenastava driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/info&path=21-2792&redirect_to=subentity&gotopage[66]=1");
 
         WebDriverWait wait3 = new WebDriverWait(driver, 5);
-        wait3.until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-primary")));
-
         WebElement taskButton = driver.findElement(By.className("btn-primary"));
+        wait3.until(ExpectedConditions.visibilityOf(taskButton));
         taskButton.click();
 
-        WebDriverWait wait4 = new WebDriverWait(driver, 5);
-        wait4.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#fields_168")));
-
         String uuid = UUID.randomUUID().toString();
+
+        WebDriverWait wait4 = new WebDriverWait(driver, 5);
+        wait4.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_168")));
         WebElement tasknameInput = driver.findElement(By.id("fields_168"));
+  //      wait4.until(ExpectedConditions.visibilityOf(tasknameInput));
+  //    pokud se wait until vola az nyni, objevuje se vyjimka unable to locate element
         tasknameInput.sendKeys("xrotm01"+uuid+"Task");
 
         WebElement tasktypeSelect = driver.findElement(By.id("fields_167"));
@@ -119,8 +114,6 @@ public class TaskTest {
         Select select3 = new Select(taskprioSelect);
         select3.selectByIndex(2);
 
-        //type Task, name, status New, prio Medium and some description
-
         WebElement descriptionIFrame = driver.findElement(By.cssSelector(".cke_wysiwyg_frame"));
         driver.switchTo().frame(descriptionIFrame);
         WebElement descriptionBody = driver.findElement(By.cssSelector(".cke_contents_ltr"));
@@ -133,8 +126,10 @@ public class TaskTest {
         // then
 
         WebDriverWait wait5 = new WebDriverWait(driver, 5);
-        wait4.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table-hover")));
+        wait5.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table-hover")));
         WebElement taskTable = driver.findElement(By.cssSelector(".table-hover"));
+  //    wait5.until(ExpectedConditions.visibilityOf(taskTable));
+  //    pokud se wait until vola az nyni, objevuje se vyjimka unable to locate element
         String innerHTML = taskTable.getAttribute("innerHTML");
         Assert.assertTrue(innerHTML.contains(uuid));
     }
@@ -156,16 +151,14 @@ public class TaskTest {
         WebElement navBarToggle = driver.findElement(By.className("navbar-toggle"));
         navBarToggle.click();
 
-        //najit radek, kliknout na nej fa fa-reorder
+        //najit radek, kliknout na nej
 
-       /*     List<WebElement> menuRows = driver.findElements(By.cssSelector("ul.page-sidebar-menu>li"));
+        List<WebElement> menuRows = driver.findElements(By.cssSelector("ul.page-sidebar-menu>li"));
         for (WebElement e: menuRows) {
             if (e.getText().equals("Projects")) {
                 e.click();
             }
-        } */
-
-        driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/items&path=21");
+        }
 
         // je treba odstranit problem s exception: element is not attached to the page document
         // pokud se pouzije vyse primo tento odkaz, problem nenastava driver.get("http://digitalnizena.cz/rukovoditel/index.php?module=items/items&path=21");
@@ -188,7 +181,7 @@ public class TaskTest {
         WebElement projectLink = driver.findElement(By.cssSelector(".item_heading_link"));
         projectLink.click();
 
-  /*      for(int i = 0;i<7;i++) {
+        for(int i = 0;i<7;i++) {
 
             WebElement taskButton = driver.findElement(By.className("btn-primary"));
             taskButton.click();
@@ -203,19 +196,14 @@ public class TaskTest {
             modalButton.click();
 
         }
-*/
-        // then
-        List<WebElement> taskItems = driver.findElements(By.cssSelector(".table-hover tr"));
- //       Assert.assertEquals(4, taskItems.size());
 
+        // then
+        WebDriverWait wait3 = new WebDriverWait(driver, 5);
+        wait3.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table-hover tr")));
+        List<WebElement> taskItems = driver.findElements(By.cssSelector(".table-hover tr"));
+        Assert.assertEquals(4, taskItems.size());
 
         // when
-
-   /*     WebDriverWait wait20 = new WebDriverWait(driver, 10);
-        wait20.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-trash-o")));
-        WebElement resetFilter = driver.findElement(By.cssSelector(".fa-trash-o"));
-        resetFilter.click();
-*/
 
         WebElement editFilter = driver.findElement(By.cssSelector(".filters-preview-condition-include"));
         editFilter.click();
@@ -231,21 +219,15 @@ public class TaskTest {
         modalButton2.click();
 
         // then
-
+        WebDriverWait wait22 = new WebDriverWait(driver, 5);
+        wait22.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table-hover tr")));
         List<WebElement> taskItems2 = driver.findElements(By.cssSelector(".table-hover tr"));
-//        Assert.assertEquals(3, taskItems2.size());
+        Assert.assertEquals(3, taskItems2.size());
         for (WebElement g: taskItems2)
             if (g.getAttribute("innerHTML").contains("Open"))
                 Assert.assertEquals(4, 3);;
 
-
         // when
-
- /*       WebDriverWait wait22 = new WebDriverWait(driver, 10);
-        wait22.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa fa-trash-o")));
-        WebElement deleteFilterButton = driver.findElement(By.cssSelector(".fa fa-trash-o"));
-        deleteFilterButton.click();
-*/
 
         WebElement editFilter2 = driver.findElement(By.cssSelector(".filters-preview-condition-include"));
         editFilter2.click();
@@ -262,40 +244,28 @@ public class TaskTest {
         modalButton3.click();
 
         // then
+        WebDriverWait wait32 = new WebDriverWait(driver, 5);
+        wait32.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table-hover tr")));
         List<WebElement> taskItems3 = driver.findElements(By.cssSelector(".table-hover tr"));
-  //      Assert.assertEquals(7, taskItems3.size());
-
+        Assert.assertEquals(8, taskItems3.size());
 
         // when
-        WebDriverWait wait41 = new WebDriverWait(driver, 5);
+        WebDriverWait wait41 = new WebDriverWait(driver, 10);
         wait41.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".select_all_items")));
         WebElement selectAllItemsButton = driver.findElement(By.cssSelector(".select_all_items"));
         selectAllItemsButton.click();
+        // trvale dochazi k time out a unable to locate u elementu .select_all_items, pritom v jQuery totez funguje
 
-  //     WebElement withSelectedButton = driver.findElement(By.cssSelector(".btn btn-default dropdown-toggle"));
         WebElement taskdeleteSelect = driver.findElement(By.cssSelector(".btn btn-default dropdown-toggle"));
         Select select3 = new Select(taskdeleteSelect);
         select3.selectByIndex(1);
 
         // then
         List<WebElement> taskItems4 = driver.findElements(By.cssSelector(".table-hover tr"));
-        Assert.assertEquals(0, taskItems4.size());
+        Assert.assertEquals(1, taskItems4.size());
 
     }
 
 
 }
 
-/*
- - TC: Project with status New, priority High and filled start date as today is created. Verify that there is new row in project table. Delete project after test.
-    Test Suite - Tasks
- - TC: shouldCreateNewTask (Precondition - there exists project yourname already in the system.) New Task will be created with type Task, name, status New, prio Medium and some description. Verify task attributes (Type Task, description, name, priority, status) on task info page (icon i). Delete that task.
- - TC: (Precondition - there exists project yourname already in the system.)
-    Create new 7 tasks with different statuses New, Open, Waiting, Done, Closed, Paid, Canceled.
-    Verify that using default filter (New, Open, Waiting) only 3 tasks will be shown.
-    Change applied filter in Filter info dialog to only contain (New, Waiting)
-    ...there are more ways how to do it (you can click small x on Open "label" to delete it,
-    or you can deal with writing into "suggestion box").
-    Verify only New and Waiting tasks are displayed. Now remove all filters and verify
-     all created tasks are displayed. Delete all tasks using Select all and batch delete.
-*/
