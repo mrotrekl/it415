7fa9e3b3674f707d8d6607cf29a9dcf272aaa507
Marek Rotrekl <bbckt@rotrekl.name>
2020-01-19 12:38:25 +0100

    travis test

--- src/test/java/cz/xrotm01/rukovoditel/selenium/TaskTest.java
+++ src/test/java/cz/xrotm01/rukovoditel/selenium/TaskTest.java
@@ -198,7 +198,27 @@ public class TaskTest {
         WebElement projectLink = driver.findElement(By.cssSelector(".item_heading_link"));
         projectLink.click();
 
+  /*      for(int i = 0;i<7;i++) {
 
+            WebElement taskButton = driver.findElement(By.className("btn-primary"));
+            taskButton.click();
+            WebDriverWait wait3 = new WebDriverWait(driver, 10);
+            wait3.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#fields_168")));
+            WebElement tasknameInput = driver.findElement(By.id("fields_168"));
+            tasknameInput.sendKeys("xrotm01Task");
+            WebElement taskstatusSelect = driver.findElement(By.id("fields_169"));
+            Select select2 = new Select(taskstatusSelect);
+            select2.selectByIndex(i);
+            WebElement modalButton = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
+            modalButton.click();
+
+        }
+
+*/
+
+
+
+/*
         WebElement taskButton = driver.findElement(By.className("btn-primary"));
         taskButton.click();
         WebDriverWait wait3 = new WebDriverWait(driver, 10);
@@ -211,7 +231,7 @@ public class TaskTest {
         WebElement modalButton = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
         modalButton.click();
 
-/*        WebElement taskButton12 = driver.findElement(By.className("btn-primary"));
+        WebElement taskButton12 = driver.findElement(By.className("btn-primary"));
         taskButton12.click();
         WebDriverWait wait12 = new WebDriverWait(driver, 10);
         wait12.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#fields_168")));
@@ -282,121 +302,103 @@ public class TaskTest {
         select17.selectByIndex(6);
         WebElement modalButton17 = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
         modalButton17.click();
+
+
+        // then
+        List<WebElement> taskItems = driver.findElements(By.cssSelector(".table-hover tr"));
+        Assert.assertEquals(4, taskItems.size());
+
+ */
+
+
+/*
+ - TC: Project with status New, priority High and filled start date as today is created. Verify that there is new row in project table. Delete project after test.
+    Test Suite - Tasks
+ - TC: shouldCreateNewTask (Precondition - there exists project yourname already in the system.) New Task will be created with type Task, name, status New, prio Medium and some description. Verify task attributes (Type Task, description, name, priority, status) on task info page (icon i). Delete that task.
+ - TC: (Precondition - there exists project yourname already in the system.)
+    Create new 7 tasks with different statuses New, Open, Waiting, Done, Closed, Paid, Canceled.
+    Verify that using default filter (New, Open, Waiting) only 3 tasks will be shown.
+    Change applied filter in Filter info dialog to only contain (New, Waiting)
+    ...there are more ways how to do it (you can click small x on Open "label" to delete it,
+    or you can deal with writing into "suggestion box").
+    Verify only New and Waiting tasks are displayed. Now remove all filters and verify
+     all created tasks are displayed. Delete all tasks using Select all and batch delete.
 */
 
+        // when
 
+   /*     WebDriverWait wait20 = new WebDriverWait(driver, 10);
+        wait20.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-trash-o")));
+        WebElement resetFilter = driver.findElement(By.cssSelector(".fa-trash-o"));
+        resetFilter.click();
+*/
 
+        WebElement editFilter = driver.findElement(By.cssSelector(".filters-preview-condition-include"));
+        editFilter.click();
 
+   //     WebElement findChoice = driver.findElement(By.cssSelector(".search-choice"));
+   //     findChoice.click();
 
-        // Then
+        WebDriverWait wait100 = new WebDriverWait(driver, 5);
+        wait100.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".search-choice-close")));
+        List<WebElement> statusChoiceX = driver.findElements(By.cssSelector(".search-choice-close"));
+        statusChoiceX.get(1).click();
 
-/*        WebElement firstRow = depositRows.get(0);
-        String innerHTML = firstRow.getAttribute("innerHTML");
-
-        if (innerHTML.contains(uuid)) {
-            Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
-            Assert.assertTrue(innerHTML.contains(depositComment));
-            return true;     // expected condition is met
-        } else {
-            return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
+/*        List<WebElement> statusChoices = driver.findElements(By.cssSelector(".search-choice"));
+        int i = 0;
+        for (WebElement e: statusChoices) {
+            if (e.getAttribute("innerHTML").contains("Open")) {
+                WebDriverWait wait100 = new WebDriverWait(driver, 1);
+                List<WebElement> statusChoiceX = driver.findElements(By.cssSelector(".search-choice-close"));
+                WebDriverWait wait101 = new WebDriverWait(driver, 1);
+                statusChoiceX.get(i).click();
+            }
+            i++;
         }
 */
 
+        WebDriverWait wait21 = new WebDriverWait(driver, 10);
+        wait21.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-primary-modal-action")));
+        WebElement modalButton2 = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
+        modalButton2.click();
 
-   /*     if (innerHTML.contains(uuid)) {
-            Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
-            Assert.assertTrue(innerHTML.contains(depositComment));
-            return true;     // expected condition is met
-        } else {
-            return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
-        } */
+        // then
 
-        // Then
-
-/*        WebElement firstRow = depositRows.get(0);
-        String innerHTML = firstRow.getAttribute("innerHTML");
-
-        if (innerHTML.contains(uuid)) {
-            Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
-            Assert.assertTrue(innerHTML.contains(depositComment));
-            return true;     // expected condition is met
-        } else {
-            return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
-        }
-*/
+        List<WebElement> taskItems2 = driver.findElements(By.cssSelector(".table-hover tr"));
+//        Assert.assertEquals(3, taskItems2.size());
+        for (WebElement g: taskItems2)
+            if (g.getAttribute("innerHTML").contains("Open"))
+                Assert.assertEquals(4, 3);;
 
 
+        // when
 
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("$('#btn-primary').click()");
+        WebDriverWait wait22 = new WebDriverWait(driver, 10);
+        wait22.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa fa-trash-o")));
+        WebElement deleteFilterButton = driver.findElement(By.cssSelector(".fa fa-trash-o"));
+        deleteFilterButton.click();
 
-//        class="btn btn-primary btn-primary-modal-action"
+        // then
+        List<WebElement> taskItems = driver.findElements(By.cssSelector(".table-hover tr"));
+        Assert.assertEquals(7, taskItems.size());
 
 
-/*       for (WebElement row : menuRows) {
-            row.click();
-        }
+        // when
+        WebElement selectAllItemsButton = driver.findElement(By.cssSelector(".select_all_items"));
+        selectAllItemsButton.click();
 
-*/
-        // Then
+        WebElement withSelectedButton = driver.findElement(By.cssSelector(".btn btn-default dropdown-toggle"));
+        //doplnit výběr smazání
 
-/*        WebElement firstRow = depositRows.get(0);
-        String innerHTML = firstRow.getAttribute("innerHTML");
 
-        if (innerHTML.contains(uuid)) {
-            Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
-            Assert.assertTrue(innerHTML.contains(depositComment));
-            return true;     // expected condition is met
-        } else {
-            return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
-        }
-*/
+        // then
+        List<WebElement> taskItems3 = driver.findElements(By.cssSelector(".table-hover tr"));
+        Assert.assertEquals(0, taskItems3.size());
 
 
 
-//        WebElement errorMessageSpan = driver.findElement(By.id("spanMessage"));
-        //   Assert.assertEquals("Invalid credentials", errorMessageSpan.getText());
-
-        // validation error exists
-        // url changed to https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials
-        // there is no menu
-
-
-        //       Assert.assertEquals("No match", errorMessageDiv.getText());
-
-        // validation error exists
-        // url changed to https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials
-        // there is no menu
-
-
-
-
-
-        // Then
-
-/*        WebElement firstRow = depositRows.get(0);
-        String innerHTML = firstRow.getAttribute("innerHTML");
-
-        if (innerHTML.contains(uuid)) {
-            Assert.assertTrue(innerHTML.contains("10-30-18"));    // beware, different date format in table grid vs. input field
-            Assert.assertTrue(innerHTML.contains(depositComment));
-            return true;     // expected condition is met
-        } else {
-            return false;    // selenium webdriver will continue polling the DOM each 500ms and check the expected condition by calling method apply(webDriver) again
-        }
-*/
-//        Assert.assertTrue(driver.getTitle().contains("Tasks"));
-
-        //       List<WebElement> depositRows = driver.findElements(By.cssSelector("#depositsTable_wrapper #depositsTable tbody tr"));
-        //       WebElement firstRow = depositRows.get(0);
 
     }
-
-
-
-
-
-
 
 
 }
